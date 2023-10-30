ac71a9bc873bae878ea90d4d962730868e194d0a
itBeginnerr <sanggminyo@gmail.com>
2023-10-30 17:33:31 +0900

    14.5 ConstrainedBox

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/breakpoints.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
@@ -41,15 +42,21 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return DefaultTabController(
       length: tabs.length,
       child: Scaffold(
         resizeToAvoidBottomInset: false,
         appBar: AppBar(
-          title: CupertinoSearchTextField(
-            controller: _textEditingController,
-            onChanged: _onSearchChanged,
-            onSubmitted: _onSearchSubmited,
+          title: ConstrainedBox(
+            constraints: const BoxConstraints(
+              maxWidth: Breakpoints.sm,
+            ),
+            child: CupertinoSearchTextField(
+              controller: _textEditingController,
+              onChanged: _onSearchChanged,
+              onSubmitted: _onSearchSubmited,
+            ),
           ),
           bottom: TabBar(
             padding: const EdgeInsets.symmetric(horizontal: Sizes.size14),
@@ -78,76 +85,80 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
               padding: const EdgeInsets.all(
                 Sizes.size5,
               ),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: width > Breakpoints.lg ? 5 : 2,
                 mainAxisSpacing: Sizes.size10,
                 crossAxisSpacing: Sizes.size10,
                 childAspectRatio: 9 / 20,
               ),
-              itemBuilder: (context, index) => Column(
-                children: [
-                  Container(
-                    clipBehavior: Clip.hardEdge,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(
-                        Sizes.size4,
+              itemBuilder: (context, index) => LayoutBuilder(
+                builder: (context, constraints) => Column(
+                  children: [
+                    Container(
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(
+                          Sizes.size4,
+                        ),
+                      ),
+                      child: AspectRatio(
+                        aspectRatio: 9 / 16,
+                        child: FadeInImage.assetNetwork(
+                            fit: BoxFit.cover,
+                            placeholder: "assets/images/beautiful_image.jpg",
+                            image:
+                                "https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?cs=srgb&dl=pexels-pixabay-326055.jpg&fm=jpg"),
                       ),
                     ),
-                    child: AspectRatio(
-                      aspectRatio: 9 / 16,
-                      child: FadeInImage.assetNetwork(
-                          fit: BoxFit.cover,
-                          placeholder: "assets/images/beautiful_image.jpg",
-                          image:
-                              "https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?cs=srgb&dl=pexels-pixabay-326055.jpg&fm=jpg"),
+                    Gaps.v5,
+                    const Text(
+                      "Im Sangmin and i will be free manasdasdas dasd asd",
+                      style: TextStyle(
+                        fontSize: Sizes.size16 + Sizes.size2,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
                     ),
-                  ),
-                  Gaps.v5,
-                  const Text(
-                    "Im Sangmin and i will be free manasdasdas dasd asd",
-                    style: TextStyle(
-                      fontSize: Sizes.size16 + Sizes.size2,
-                      fontWeight: FontWeight.w600,
-                    ),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  Gaps.v5,
-                  DefaultTextStyle(
-                    style: TextStyle(
-                      color: Colors.grey.shade500,
-                      fontWeight: FontWeight.w600,
-                    ),
-                    child: const Row(
-                      children: [
-                        CircleAvatar(
-                          radius: 14,
-                          child: Text(
-                            "상민",
-                            style: TextStyle(
-                              fontSize: Sizes.size12,
+                    Gaps.v5,
+                    if (constraints.maxWidth < 200 ||
+                        constraints.maxWidth > 250)
+                      DefaultTextStyle(
+                        style: TextStyle(
+                          color: Colors.grey.shade500,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        child: const Row(
+                          children: [
+                            CircleAvatar(
+                              radius: 14,
+                              child: Text(
+                                "상민",
+                                style: TextStyle(
+                                  fontSize: Sizes.size12,
+                                ),
+                              ),
                             ),
-                          ),
+                            Gaps.h5,
+                            Expanded(
+                              child: Text(
+                                "dfjldkajflkjjafljldksfjkllaksdjkldas",
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                            Gaps.h5,
+                            FaIcon(
+                              FontAwesomeIcons.heart,
+                              size: Sizes.size14,
+                            ),
+                            Gaps.h2,
+                            Text("2.5M"),
+                          ],
                         ),
-                        Gaps.h5,
-                        Expanded(
-                          child: Text(
-                            "dfjldkajflkjjafljldksfjkllaksdjkldas",
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                        Gaps.h5,
-                        FaIcon(
-                          FontAwesomeIcons.heart,
-                          size: Sizes.size14,
-                        ),
-                        Gaps.h2,
-                        Text("2.5M"),
-                      ],
-                    ),
-                  )
-                ],
+                      )
+                  ],
+                ),
               ),
             ),
             for (var tab in tabs.skip(1))
